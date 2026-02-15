78c15c66e0e15304f31ac1a0265e0b0a162e49a6
Diego Fernández Arenas <dferare1310@g.educaand.es>
2026-02-15 11:44:20 +0100

    Refactor: Nombres limpios y métodos extraídos en Inventario

--- src/main/java/com/gameshop/GestorInventario.java
+++ src/main/java/com/gameshop/GestorInventario.java
@@ -2,20 +2,26 @@ package com.gameshop;
 
 public class GestorInventario {
 
-    public void p(String[] n, int[] c) {
-        int t = 0;
+    public static final int stockMinimo = 5;
 
-        // BUG LÓGICO: El '- 1' hace que el último elemento (Halo) se ignore por completo.
+    public void MostrarStock(String[] n, int[] c) {
+        int TotalJuegos = 0;
+
+        TotalJuegos = getJuegos(n, c, TotalJuegos);
+        System.out.println("Total de juegos en el almacén: " + TotalJuegos);
+    }
+
+    private static int getJuegos(String[] n, int[] c, int TotalJuegos) {
+
         for (int i = 0; i < n.length; i++) {
             System.out.println("Revisando stock de: " + n[i]);
 
-            // CODE SMELL: Magic Number (5)
-            if (c[i] <= 5) {
+            if (c[i] <= stockMinimo) {
                 System.out.println("¡ALERTA! Pedir más copias de " + n[i]);
             }
 
-            t = t + c[i];
+            TotalJuegos = TotalJuegos + c[i];
         }
-        System.out.println("Total de juegos en el almacén: " + t);
+        return TotalJuegos;
     }
 }

2cf9d3618cbe04d7abea35a7fb87f0fe17bdb30d
Fran <ffermeg@g.educaand.es>
2026-02-19 22:31:17 +0100

    Refactor: Nombres limpios y métodos extraídos en

--- src/main/java/com/gameshop/ProcesadorVentas.java
+++ src/main/java/com/gameshop/ProcesadorVentas.java
@@ -2,23 +2,35 @@ package com.gameshop;
 
 public class ProcesadorVentas {
 
+    public static final double descuento = 0.15;
+    public static final double IVA = 1.21;
     // VULNERABILIDAD SONARLINT: Contraseña en texto plano
     private String dbClave = "admin_super_secreto_123";
 
-    public double c(double p, boolean v) {
+    public double c(double precio, boolean venta) {
         double d = 0;
 
-        if (v) {
+        if (venta) {
             // CODE SMELL: Magic Number (0.15 representa el 15% de descuento)
-            d = p * 0.15;
+            d = Descuento(precio);
         }
 
         // BUG LÓGICO: Al VIP se le está RESTANDO el descuento al precio original (CORREGIDO)
-        double pf = p - d;
+        double precioFinal = precio - d;
 
         // CODE SMELL: Magic Number (1.21 representa el 21% de IVA)
-        pf = pf * 1.21;
+        precioFinal = CalculoIVA(precioFinal);
 
-        return pf;
+        return precioFinal;
+    }
+
+    private static double CalculoIVA(double precioFinal) {
+        return precioFinal * IVA;
+    }
+
+    private static double Descuento(double precio) {
+        double d;
+        d = precio * descuento;
+        return d;
     }
 }
